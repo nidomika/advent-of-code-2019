be15c1cdd30e03c6becf870a1ba23c290d6ab913
Dominika Szymańska <34137726+nidomika@users.noreply.github.com>
2019-12-03 23:18:29 +0100

    Update aoc3.py

--- Day 3/aoc3.py
+++ Day 3/aoc3.py
@@ -6,8 +6,6 @@ for number in lines:
     wires.append(number)
 wires = [x.split(',') for x in wires]
 
-#wires = [['R75','D30','R83','U83','L12','D49','R71','U7','L72'],['U62','R66','U55','R34','D71','R55','D58','R83']]
-
 def manhattan_distance(point):
     return sum(map(abs, point))
 
@@ -16,7 +14,6 @@ def move(wire):
     y = 0
     j = 1
     path = {}
-    path1 = [[],[]]
     for instruction in wire:
         distance = int(instruction[1:])
         for step in range(distance):
@@ -35,8 +32,6 @@ def move(wire):
 wire1 = move(wires[0])
 wire2 = move(wires[1])
 intersections = set(move(wires[0])).intersection(set(move(wires[1])))
-
-
 
 manhattan = {
     manhattan_distance(point): wire1[point] + wire2[point]
